03141ecb4aaac4c7a93306d650983177c89c7042
Koustubh Shrivastava <koustubhs@XEBIAINDIA.LOCAL>
2018-11-22 12:19:20 +0530

    Styling the header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,10 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'] 
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'Demonstrations';
   currentRoute = '';
 
   constructor(location: Location, router: Router) {
